0149693adef87aa0bee7e1a352d03d1f1d1c0126
Mertcan Karataş <can.mert.karatas@gmail.com>
2021-11-26 22:08:39 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/UserDal.cs
@@ -0,0 +1,63 @@
+﻿using Core.DataAccess.InMemory;
+using Core.Entities;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+   public class UserDal:IUserDal
+    {
+       public List<User> _users;
+        public UserDal()
+        {
+            _users = new List<User>
+            {
+                new User{UserId=1,UserName="Mertcan",Email="can.mert.karatas@gmail.com",password="a1b2c3d4e5"},
+                new User{UserId=1,UserName="Burak",Email="burak@gmail.com",password="a1b2c3d4e5"},
+                new User{UserId=1,UserName="Can",Email="can@gmail.com",password="a1b2c3d4e5"},
+                new User{UserId=1,UserName="Veli",Email="veli@gmail.com",password="a1b2c3d4e5"},
+                new User{UserId=1,UserName="Ali",Email="ali@gmail.com",password="a1b2c3d4e5"}
+            };
+        }
+
+        public void Add(User entity)
+        {
+            _users.Add(entity);
+        }
+
+        public void Delete(User entity)
+        {
+            var itemRemove = _users.Single(d => d.UserId == entity.UserId);
+            _users.Remove(itemRemove);
+        }
+
+        public List<User> Get(Expression<Func<User, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(int id)
+        {
+            return _users.SingleOrDefault(u => u.UserId == id);
+        }
+
+        public List<User> Getall()
+        {
+            return _users.ToList();
+        }
+
+        public void Update(User entity)
+        {
+            var userUpdate = _users.SingleOrDefault(u => u.UserId == entity.UserId);
+            userUpdate.UserName = entity.UserName;
+            userUpdate.Email = entity.Email;
+            userUpdate.password = entity.password;
+        }
+    }
+}
---
+++ Entity/Concrete/User.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Entities.Concrete
+{
+    public class User:IEntity
+    {   
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(8)]
+        public string password { get; set; }
+
+    }
+}
